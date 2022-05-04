19e96338b54387819aef6447fca9fc9d01d4e443
Utkarsh Yadav <utkarshrock1510@gmail.com>
2022-05-04 22:13:45 +0530

    Fixing some issues

--- src/test/java/com/utkarsh/UserManagement/UserManagementProjectApplicationTests.java
+++ src/test/java/com/utkarsh/UserManagement/UserManagementProjectApplicationTests.java
@@ -2,8 +2,6 @@ package com.utkarsh.UserManagement;
 
 import com.utkarsh.UserManagement.model.User;
 import com.utkarsh.UserManagement.repository.UserRepository;
-import com.utkarsh.UserManagement.service.UserService;
-import com.utkarsh.UserManagement.service.UserServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -18,33 +16,40 @@ class UserManagementProjectApplicationTests {
   @Autowired
 	UserRepository repository;
 
-	UserServiceImpl userService;
-	@Test
-	String RegisterUser() {
 
-		User user = new User();
-		User user1 = new User();
-		user.setUsername("hello-utkarsh");
-		user.setFirstName("Utkarsh");
-		user.setLastName("yadav");
-		user.setEmail("contactutkars18@gmail.com");
-		user1.setUsername("hello-rishav");
-		user1.setFirstName("Rishav");
-	    user1.setLastName("Raj");
-		user1.setEmail("riv@gmail.com");
-		repository.save(user);
-		repository.save(user1);
-		if(repository.findById(1).get().getUsername()=="hello-utkarsh" ) {
+         String adduser(){
+
+			 User user = new User();
+			 User user1 = new User();
+			 user.setUsername("hello-utkarsh");
+			 user.setFirstName("Utkarsh");
+			 user.setLastName("yadav");
+			 user.setEmail("contactutkars18@gmail.com");
+			 user1.setUsername("hello-rishav");
+			 user1.setFirstName("Rishav");
+			 user1.setLastName("Raj");
+			 user1.setEmail("riv@gmail.com");
+			 repository.save(user);
+			 repository.save(user1);
+
+			 return ( repository.findAll().toString());
+
+
+		 }
+
+	@Test
+	void RegisterUser() {
+			 adduser();
+		if(repository.findById(1).isPresent() ) {
 			System.out.println("User Registered successfully");
 		}
 		else{
 			System.out.println("Error");
 		}
-		return repository.findAll().toString();
 	}
 	@Test
 	void fetchUser(){
-		RegisterUser();
+			 adduser();
 		List<User> list;
 	  	list = repository.findAll();
 		System.out.println(list);
@@ -58,9 +63,10 @@ class UserManagementProjectApplicationTests {
 
 	@Test
 	void deleteAllUser(){
+			 adduser();
 		repository.deleteAll();
 		if(repository.findAll().isEmpty()){
-			System.out.println("No user found");
+			System.out.println("All users Deleted");
 
 		}
 		else{
